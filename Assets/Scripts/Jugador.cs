0a3066553811da96fc23b8ee07f319988ea0ac3b
Manolo <manolojm96@gmail.com>
2023-05-08 12:04:59 +0200

    Tiempo

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Jugador : MonoBehaviour {
@@ -7,9 +8,14 @@ public class Jugador : MonoBehaviour {
     private float velocidad = 4.0f;
     public GameObject pantallaFin;
 
+    public TextMeshProUGUI tiempoTxt;
+    private float tiempoInicio;
+    private int tiempoEmpleado;
+
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody>();
+        tiempoInicio = Time.time;
     }
 
     // Update is called once per frame
@@ -19,13 +25,26 @@ public class Jugador : MonoBehaviour {
         float movimientoV = Input.GetAxis("Vertical");
 
         transform.position += new Vector3(movimientoH, 0, movimientoV) * velocidad * Time.deltaTime;
+
+        // Actualizamos tiempo
+        tiempoEmpleado = (int)Time.time - (int)tiempoInicio;
+        SetTiempo(tiempoEmpleado);
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Enemigo")) {
+            // Pausa
             Time.timeScale = 0;
+
+            // Fin
             pantallaFin.SetActive(true);
             Debug.Log("Fin");
         }
+    }
+
+    public void SetTiempo(int tiempo) {
+        int segundos = tiempo % 60;
+        int minutos = tiempo / 60;
+        tiempoTxt.text = minutos.ToString("00") + ":" + segundos.ToString("00");
     }
 }
